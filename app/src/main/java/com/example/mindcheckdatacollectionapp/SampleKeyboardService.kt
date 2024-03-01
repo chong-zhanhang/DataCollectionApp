c25c1cae35a35ba92696b332031ff8d721131b7e
chong-zhanhang <chongzhanhang@hotmail.com>
2024-03-01 18:32:31 +0800

    Add Firebase Auth, Firestore, and Storage SDK

--- app/src/main/java/com/example/mindcheckdatacollectionapp/SampleKeyboardService.kt
+++ app/src/main/java/com/example/mindcheckdatacollectionapp/SampleKeyboardService.kt
@@ -1,4 +1,16 @@
 package com.example.mindcheckdatacollectionapp
 
-class SampleKeyboardService {
+import co.thingthing.fleksy.core.keyboard.KeyboardConfiguration
+import co.thingthing.fleksy.core.keyboard.KeyboardService
+
+class SampleKeyboardService : KeyboardService() {
+
+    override fun createConfiguration(): KeyboardConfiguration {
+        return KeyboardConfiguration(
+            license = KeyboardConfiguration.LicenseConfiguration(
+                licenseKey = "4d81689f-302f-4611-80de-55d4051cdd2c",
+                licenseSecret = "df1c1a75664cf18ed71164542390187d"
+            )
+        )
+    }
 }
